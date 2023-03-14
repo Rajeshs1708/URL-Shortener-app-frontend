37a3745bd777361439e05e73d58942a50e06ea2d
Rajesh <rajeshs1097@gmail.com>
2023-03-14 11:50:44 +0530

    first commit

--- src/Profile.js
+++ src/Profile.js
@@ -1,118 +1,145 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import Navbar from './Navbar';
-import './Profile.css';
-import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import Navbar from './Navbar'
+import './Profile.css'
+import { useNavigate } from 'react-router-dom'
 
-function Profile() {
-
-  const navigate = useNavigate();
-  const [urlList, setUrlList] = useState([]);
-  const [longUrl, setLongUrl] = useState('');
+function Profile () {
+  const navigate = useNavigate()
+  const [urlList, setUrlList] = useState([])
+  const [longUrl, setLongUrl] = useState('')
   const [error, setError] = useState('')
 
   useEffect(() => {
-    const token = localStorage.getItem('TOKEN');
+    const token = localStorage.getItem('TOKEN')
     if (!token) {
       navigate('/')
     }
   }, [navigate])
 
   useEffect(() => {
-    getUrlList();
-  }, []);
+    getUrlList()
+  }, [])
 
   const getUrlList = () => {
-    axios.get(`${process.env.REACT_APP_BASE_URL}/api/getUrl`)
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/api/getUrl`)
       .then(response => {
         setUrlList(response.data)
-      }).catch(err => {
+      })
+      .catch(err => {
         console.log('Error:', err)
       })
-  };
+  }
 
-  const deleteUrl = (urlId) => {
-    axios.delete(`${process.env.REACT_APP_BASE_URL}/api/deleteUrl/${urlId}`).then(response => {
-      if (response) {
-        getUrlList();
-      }
-    }).catch(err => {
-      console.log('Err: ', err);
-    })
-  };
+  const deleteUrl = urlId => {
+    axios
+      .delete(`${process.env.REACT_APP_BASE_URL}/api/deleteUrl/${urlId}`)
+      .then(response => {
+        if (response) {
+          getUrlList()
+        }
+      })
+      .catch(err => {
+        console.log('Err: ', err)
+      })
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = e => {
+    e.preventDefault()
     try {
       if (longUrl) {
-        axios.post(`${process.env.REACT_APP_BASE_URL}/api/create`, { longUrl: longUrl })
+        axios
+          .post(`${process.env.REACT_APP_BASE_URL}/api/create`, {
+            longUrl: longUrl
+          })
           .then(res => {
-            setError(res.data.message);
+            setError(res.data.message)
             setLongUrl('')
-            getUrlList();
+            getUrlList()
             setTimeout(() => {
-              setError('');
+              setError('')
             }, 2000)
-
-
           })
           .catch(err => {
             setError(err.response.data.message)
             setTimeout(() => {
-              setError('');
+              setError('')
             }, 2000)
           })
-
       } else {
-        setError("Invalid input")
+        setError('Invalid input')
         setTimeout(() => {
-          setError('');
+          setError('')
         }, 2000)
       }
     } catch (err) {
-      console.log("Error...", err);
+      console.log('Error...', err)
     }
   }
-
-
 
   return (
     <div className='container-fluid'>
       <Navbar />
+      {error && <p className='alert alert-success'>{error}</p>}
 
       <div className='inputSession row'>
         <form onSubmit={handleSubmit}>
-          <div className="input-group">
-            <input value={longUrl} onChange={e => setLongUrl(e.target.value)} type="text" className="form-control" placeholder="Paste your long URL" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-            <div className="input-group-append">
-              <input className="btn btn-primary" type="submit" />
+          <div className='input-group'>
+            <input
+              value={longUrl}
+              onChange={e => setLongUrl(e.target.value)}
+              type='text'
+              className='form-control'
+              placeholder='Paste your long URL'
+              aria-label="Recipient's username"
+              aria-describedby='basic-addon2'
+            />
+            <div className='input-group-append'>
+              <input className='btn btn-success p-3' type='submit' />
             </div>
           </div>
           <br />
-          {error && <p className='alert alert-success'>{error}</p>}
         </form>
       </div>
 
       <div className='row urlList'>
         {urlList.map((url, index) => (
-          <div key={index} className='col-lg-3 col-md-4 col-sm-12'>
-            <div className="card border-secondary mb-3" style={{ maxWidth: "18rem" }}>
-              <div className="card-header"><h5>Total Click : {url.clickCount}</h5></div>
-              <div className="card-body text-secondary">
-                <h6 className="card-title p-2">
-                  Short URL : <a href={`${process.env.REACT_APP_BASE_URL}/api/${url.shortUrl}`} className='urlHeading' target='_blank' without rel="noreferrer">{process.env.REACT_APP_BASE_URL}/api/{url.shortUrl}</a>
+          <div key={index} className='col-lg-4 col-md-6 col-sm-12'>
+            <div
+              className='card border-secondary mb-3'
+              style={{ maxWidth: '18rem' }}
+            >
+              <div className='card-header'>
+                <h5>Total Click : {url.clickCount}</h5>
+              </div>
+              <div className='card-body text-secondary'>
+                <h6 className='card-title p-2'>
+                  Short URL :{' '}
+                  <a
+                    href={`${process.env.REACT_APP_BASE_URL}/api/${url.shortUrl}`}
+                    className='urlHeading'
+                    target='_blank'
+                    without
+                    rel='noreferrer'
+                  >
+                    {process.env.REACT_APP_BASE_URL}/api/{url.shortUrl}
+                  </a>
                 </h6>
                 <h6 className='p-2'>
-                  Long URL : <span className="card-text urlHeading">{url.longUrl}</span>
+                  Long URL :{' '}
+                  <span className='card-text urlHeading'>{url.longUrl}</span>
                 </h6>
-                <button onClick={() => deleteUrl(url._id)} className='btn btn-danger'>Remove</button>
-
+                <button
+                  onClick={() => deleteUrl(url._id)}
+                  className='btn btn-danger'
+                >
+                  Remove
+                </button>
               </div>
             </div>
           </div>
-        ))
-        }
-
+        ))}
       </div>
     </div>
   )
